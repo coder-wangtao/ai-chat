c207a521965e5606a7269118c59725e4e2d94072
w t <1475929340@qq.com>
2025-11-11 09:40:25 +0800

    feat: init model

--- lib/ai/providers.ts
+++ lib/ai/providers.ts
@@ -5,6 +5,7 @@ import {
   wrapLanguageModel,
 } from "ai";
 import { isTestEnvironment } from "../constants";
+import { deepseek } from '@ai-sdk/deepseek';
 
 export const myProvider = isTestEnvironment
   ? (() => {
@@ -25,12 +26,12 @@ export const myProvider = isTestEnvironment
     })()
   : customProvider({
       languageModels: {
-        "chat-model": gateway.languageModel("xai/grok-2-vision-1212"),
+        "chat-model": deepseek("deepseek-chat"),
         "chat-model-reasoning": wrapLanguageModel({
-          model: gateway.languageModel("xai/grok-3-mini"),
+          model: deepseek("deepseek-reasoner"),
           middleware: extractReasoningMiddleware({ tagName: "think" }),
         }),
-        "title-model": gateway.languageModel("xai/grok-2-1212"),
-        "artifact-model": gateway.languageModel("xai/grok-2-1212"),
+        "title-model": deepseek("deepseek-chat"),
+        "artifact-model": deepseek("deepseek-chat"),
       },
     });
